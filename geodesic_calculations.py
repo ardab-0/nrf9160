c187b2de74af8b14b1434c8de35b95626aace6ce
ardabuglagil <arda.buglagil@fau.de>
2022-09-06 20:46:02 +0300

    added coordinate system conversion

--- geodesic_calculations.py
+++ geodesic_calculations.py
@@ -1,11 +1,38 @@
 from geographiclib.geodesic import Geodesic
+import numpy as np
 
 
-inverse_dict = Geodesic.WGS84.Inverse(-41.32, 174.81, 40.96, -5.50)
-distance_meter = inverse_dict["s12"]
+def get_distance_and_bearing(point_from, point_to):
+    """
 
-# (bearing) is given by azi1 (161.067... degrees clockwise from north)
-bearing_angle_deg = inverse_dict["azi1"]
-print(inverse_dict)
-print(distance_meter)
-print(bearing_angle_deg)
+    :param point_from: (latitude, longitude)
+    :param point_to: (latitude, longitude)
+    :return: distance(m), bearing (degrees clockwise from north)
+    """
+    inverse_dict = Geodesic.WGS84.Inverse(point_from[0], point_from[1], point_to[0], point_to[1])
+    distance_meter = inverse_dict["s12"]
+    bearing_angle_deg = inverse_dict["azi1"]
+    return distance_meter, bearing_angle_deg
+
+
+def adjust_angle(angle):
+    return - angle + 90
+
+
+def get_cartesian_coordinates(coordinates_np):
+    i = 1
+    cartesian_coordinates_np = np.zeros_like(coordinates_np)
+
+    while i < len(coordinates_np):
+        distance_meter, bearing_angle_deg = get_distance_and_bearing(coordinates_np[i - 1],
+                                                                     coordinates_np[i])
+        # print("Distance:", distance_meter)
+        # print("Angle", bearing_angle_deg)
+        dx = distance_meter * np.cos(np.deg2rad(bearing_angle_deg))
+        dy = distance_meter * np.sin(np.deg2rad(bearing_angle_deg))
+        # print("dx:", dx)
+        # print("dy", dy)
+        # print("\n\n")
+        cartesian_coordinates_np[i] = cartesian_coordinates_np[i - 1] + np.array([dx, dy])
+        i+=1
+    return cartesian_coordinates_np
