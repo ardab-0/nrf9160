9c8b0e37b13be361b26dff9cff14545232b42ecf
ardabuglagil <arda.buglagil@fau.de>
2022-09-07 18:47:17 +0300

    added cartesian to geographic coordinate conversion

--- geodesic_calculations.py
+++ geodesic_calculations.py
@@ -1,13 +1,12 @@
 from geographiclib.geodesic import Geodesic
 import numpy as np
-
-
+import geopy
 def get_distance_and_bearing(point_from, point_to):
     """
 
     :param point_from: (latitude, longitude)
     :param point_to: (latitude, longitude)
-    :return: distance(m), bearing (degrees clockwise from north)
+    :return: distance(m), bearing (degrees)
     """
     inverse_dict = Geodesic.WGS84.Inverse(point_from[0], point_from[1], point_to[0], point_to[1])
     distance_meter = inverse_dict["s12"]
@@ -36,3 +35,17 @@ def get_cartesian_coordinates(coordinates_np):
         cartesian_coordinates_np[i] = cartesian_coordinates_np[i - 1] + np.array([dx, dy])
         i+=1
     return cartesian_coordinates_np
+
+
+def get_coordinates(cartesian_coordinates_np, original_coordinates_np):
+    coordinates = np.zeros_like(original_coordinates_np)
+    coordinates[0] = original_coordinates_np[0]
+    i = 1
+
+    while i < len(coordinates):
+        displacement = cartesian_coordinates_np[i] - cartesian_coordinates_np[i-1]
+        dist_m = np.linalg.norm(displacement)
+        bearing_angle_deg = np.rad2deg(np.arctan2(displacement[1], displacement[0]))
+
+        next_point = geopy.distance.distance(miles=10).destination((coordinates[i-1, 0], coordinates[i-1, 1]), bearing=adjust_angle(bearing_angle_deg))
+        coordinates[i, 0], coordinates[i, 1] = next_point[0], next_point[1]
