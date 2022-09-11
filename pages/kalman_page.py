a4dd1665c4c99a213d6d4a112438e5e526e1552f
ardabuglagil <arda.buglagil@fau.de>
2022-09-11 20:09:56 +0300

    added changes to display

--- pages/kalman_page.py
+++ pages/kalman_page.py
@@ -7,40 +7,14 @@ from geodesic_calculations import get_cartesian_coordinates, get_coordinates
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 import pydeck as pdk
+from constants import S_TO_MS
 
 
-def get_kalman_matrices(measurement_sigma=1, dt=1, sigma_a=1):
-    F = np.array([[1, dt, 0.5 * dt ** 2, 0, 0, 0],
-                  [0, 1, dt, 0, 0, 0],
-                  [0, 0, 1, 0, 0, 0],
-                  [0, 0, 0, 1, dt, 0.5 * dt ** 2],
-                  [0, 0, 0, 0, 1, dt],
-                  [0, 0, 0, 0, 0, 1]
-                  ], dtype=float)
-
-    H = np.array([[1, 0, 0, 0, 0, 0],
-                  [0, 0, 0, 1, 0, 0]], dtype=float)
-
-    R = np.array([[measurement_sigma, 0],
-                  [0, measurement_sigma]], dtype=float)
-
-    Q = sigma_a ** 2 * np.array([[dt ** 4 / 4, dt ** 3 / 2, dt ** 2 / 2, 0, 0, 0],
-                                 [dt ** 3 / 2, dt ** 2, dt, 0, 0, 0],
-                                 [dt ** 2 / 2, dt, 1, 0, 0, 0],
-                                 [0, 0, 0, dt ** 4 / 4, dt ** 3 / 2, dt ** 2 / 2],
-                                 [0, 0, 0, dt ** 3 / 2, dt ** 2, dt],
-                                 [0, 0, 0, dt ** 2 / 2, dt, 1]], dtype=float)
-
-    return F, H, R, Q
-
-
-# map_radius_scale = 1 / 1
-# sigma_a = 0.03
 
 dataset_type = st.sidebar.selectbox(
      'Dataset type: ',
      ('Stationary', 'Moving'))
-sigma_a = st.sidebar.slider('Acceleration Sigma (m/s\u00b2)', 0.0, 2.0, 0.03)
+sigma_a = st.sidebar.slider('Acceleration Sigma (m/s\u00b2)', 0.0, 2.0, 0.15)
 map_radius_scale = st.sidebar.slider('Scale of radii', 0.0, 1.0, 0.03)
 
 measurement = None
@@ -86,29 +60,40 @@ moving_path_df["filtered_longitude"] = filtered_geographic_coordinates[:, 0]
 moving_path_df["filtered_measurement_uncertainties"] = measurement_uncertainties
 
 # scale map representations of radii to make them more legible in map
-moving_path_df["filtered_measurement_uncertainties"] *= map_radius_scale
-moving_path_df["Range"] *= map_radius_scale
+# moving_path_df["filtered_measurement_uncertainties"] *= map_radius_scale
+# moving_path_df["Range"] *= map_radius_scale
 
+st.write("Measurement Data")
 st.write(moving_path_df)
-st.write("Unfiltered Cartesian Coordinates")
-st.write(cartesian_coordinates)
 
-st.write("Filtered Cartesian Coordinates")
-st.write(filtered_cartesian_coordinates)
+col1, col2, col3 = st.columns(3)
 
-st.write("Filtered Geographic Coordinates")
-st.write(filtered_geographic_coordinates)
+with col1:
+    st.write("Unfiltered Cartesian Coordinates")
+    st.write(cartesian_coordinates)
 
+with col2:
+    st.write("Filtered Cartesian Coordinates")
+    st.write(filtered_cartesian_coordinates)
+with col3:
+    st.write("Filtered Geographic Coordinates")
+    st.write(filtered_geographic_coordinates)
+
+# Plot cartesian filtered and unfiltered coordinates in cartesian
 trace1 = go.Scatter(x=cartesian_coordinates[:, 0], y=cartesian_coordinates[:, 1], name="Unfiltered")
 trace2 = go.Scatter(x=filtered_cartesian_coordinates[:, 0], y=filtered_cartesian_coordinates[:, 1], name="Filtered")
 
 fig = make_subplots()
 fig.add_trace(trace1)
 fig.add_trace(trace2)
-fig['layout'].update(height=600, width=800, title="Kalman Filter in Cartesian Coordinates", xaxis=dict(title='x (m)'),
+fig['layout'].update(height=600, width=800, title="Kalman Filtered and Unfiltered Coordinates in Cartesian Coordinates", xaxis=dict(title='x (m)'),
                      yaxis=dict(title='y (m)'))
 st.plotly_chart(fig)
 
+
+# Plot coordiantes on map
+moving_path_df["filtered_measurement_uncertainties"] *= map_radius_scale
+moving_path_df["Range"] *= map_radius_scale
 unfiltered_path_layer = pdk.Layer(
     "ScatterplotLayer",
     data=moving_path_df,
@@ -143,7 +128,7 @@ filtered_path_layer = pdk.Layer(
     tooltip="test test",
 )
 
-view = pdk.ViewState(latitude=50, longitude=10, zoom=3, )
+view = pdk.ViewState(latitude=50, longitude=11, zoom=9, )
 # Create the deck.gl map
 r = pdk.Deck(
     layers=[unfiltered_path_layer, filtered_path_layer],
@@ -152,4 +137,20 @@ r = pdk.Deck(
 )
 
 # Render the deck.gl map in the Streamlit app as a Pydeck chart
+st.write("Kalman Filtered and Unfiltered Coordinates on Map")
 map = st.pydeck_chart(r)
+moving_path_df["filtered_measurement_uncertainties"] /= map_radius_scale
+moving_path_df["Range"] /= map_radius_scale
+
+st.write("")
+
+trace1 = go.Scatter(x=moving_path_df["measurement_time"] * S_TO_MS, y=moving_path_df["filtered_measurement_uncertainties"], name="filter uncertainty")
+trace2 = go.Scatter(x=moving_path_df["measurement_time"] * S_TO_MS, y=moving_path_df["Range"], name="range of base station")
+
+fig = make_subplots()
+fig.add_trace(trace1)
+fig.add_trace(trace2)
+
+fig['layout'].update(title="Uncertainty of Filter and Range of Base Station in Each Time Step", xaxis=dict(title='Measurement Time (s) (difference from modem boot time)'),
+                     yaxis=dict(title='Uncertainty / Range (m)'))
+st.plotly_chart(fig)
