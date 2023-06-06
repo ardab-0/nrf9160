726519c2e95ee20169767d81ebd26cc720e3cb99
ardabuglagil <arda.buglagil@fau.de>
2023-06-06 16:08:44 +0200

    add parametric normalization

--- nn_no_preaugmentation/test.py
+++ nn_no_preaugmentation/test.py
@@ -5,18 +5,17 @@ from nn_no_preaugmentation.dataset import MeasurementDataset
 from nn_no_preaugmentation.mlp import Mlp, get_network_prediction
 
 
-
 def get_model_predictions_on_test_dataset(restored_checkpoint, checkpoint_folder, output_classes, input_features,
                                           test_x_directory, test_y_directory,
-                                          batch_size, num_prev_steps, train_x_directory, train_y_directory):
-
+                                          batch_size, num_prev_steps, train_x_directory, train_y_directory, normalize=True):
     train_dataset = MeasurementDataset(x_directory=train_x_directory,
                                        y_directory=train_y_directory,
                                        num_features=input_features,
                                        num_prev_steps=num_prev_steps,
                                        augmentation_count=0,
                                        augmentation_distance_m=0,
-                                       is_training=True
+                                       is_training=True,
+                                       normalize=normalize
                                        )
     train_min_max = train_dataset.get_training_min_max()
 
@@ -27,7 +26,8 @@ def get_model_predictions_on_test_dataset(restored_checkpoint, checkpoint_folder
                                    augmentation_count=0,
                                    augmentation_distance_m=0,
                                    is_training=False,
-                                   training_set_min_max=train_min_max
+                                   training_set_min_max=train_min_max,
+                                   normalize=normalize
                                    )
 
     test_dataloader = DataLoader(test_data, batch_size=batch_size)
@@ -70,40 +70,27 @@ def get_model_predictions_on_test_dataset(restored_checkpoint, checkpoint_folder
     return all_predictions, all_labels, all_probabilities
 
 
-# prediction_grid_indices, label_grid_indices = get_model_predictions_on_test_dataset(
-#             restored_checkpoint=29,
-#             checkpoint_folder="./checkpoints/mlp_9_grid50_prev5",
-#             output_classes=64 * 4,
-#             input_features=9,
-#             test_x_directory="../datasets/erlangen_test_dataset_gridlen50.csv",
-#             test_y_directory="../datasets/erlangen_test_dataset_gridlen50_label.csv",
-#             batch_size=32,
-#             num_prev_steps=5)
-
 if __name__ == "__main__":
     # dataset parameters
     GRID_WIDTH = 800
     GRID_HEIGHT = 800
     grid_element_length = 50
-    num_prev_steps = 15
-    input_features = 12
-    restored_checkpoint = 299
+    num_prev_steps = 3
+    input_features = 6
+    restored_checkpoint = 250
+    normalize = True
     # dataset parameters
-
-
 
     output_classes = int((GRID_WIDTH / grid_element_length) * (GRID_HEIGHT / grid_element_length))
     network_input_length = num_prev_steps * input_features
 
-    checkpoint_folder = f"checkpoints/mlp_{input_features}_grid{grid_element_length}_prev{num_prev_steps}_normalized_minadjusted"
+    checkpoint_folder = f"checkpoints/mlp_{input_features}_grid{grid_element_length}_prev{num_prev_steps}{'_normalized' if normalize else ''}_minadjusted"
 
     train_x_directory = f"../datasets/erlangen_dataset_minadjusted_gridlen{grid_element_length}.csv"
     train_y_directory = f"../datasets/erlangen_dataset_minadjusted_gridlen{grid_element_length}_label.csv"
 
     test_x_directory = f"../datasets/erlangen_test_dataset_minadjusted_gridlen{grid_element_length}.csv"
     test_y_directory = f"../datasets/erlangen_test_dataset_minadjusted_gridlen{grid_element_length}_label.csv"
-
-
 
     get_model_predictions_on_test_dataset(restored_checkpoint=restored_checkpoint,
                                           checkpoint_folder=checkpoint_folder,
@@ -114,5 +101,6 @@ if __name__ == "__main__":
                                           batch_size=32,
                                           num_prev_steps=num_prev_steps,
                                           train_x_directory=train_x_directory,
-                                          train_y_directory=train_y_directory
+                                          train_y_directory=train_y_directory,
+                                          normalize=normalize
                                           )
--- nn_no_preaugmentation/train.py
+++ nn_no_preaugmentation/train.py
@@ -6,36 +6,33 @@ from matplotlib import pyplot as plt
 import numpy as np
 from mlp import Mlp
 
-restored_checkpoint = -1 # -1 for no restoration
+restored_checkpoint = -1  # -1 for no restoration
+epochs = 300  # train until this epoch
 
 # dataset parameters
 GRID_WIDTH = 800
 GRID_HEIGHT = 800
 grid_element_length = 50
 
-
+normalize = True
 
 # network parameters
 batch_size = 64
 learning_rate = 1e-3
-epochs = 300  # train until this epoch
 train_ratio = 0.9
-num_prev_steps = 15
-input_features = 18
+num_prev_steps = 3
+input_features = 6
 augmentation_count = 0
 augmentation_distance_m = 3
 # network parameters
 
-output_classes = int( (GRID_WIDTH / grid_element_length) * (GRID_HEIGHT / grid_element_length) )
+output_classes = int((GRID_WIDTH / grid_element_length) * (GRID_HEIGHT / grid_element_length))
 network_input_length = num_prev_steps * input_features
 
-
-checkpoint_folder = f"checkpoints/mlp_{input_features}_grid{grid_element_length}_prev{num_prev_steps}_normalized_minadjusted"
+checkpoint_folder = f"checkpoints/mlp_{input_features}_grid{grid_element_length}_prev{num_prev_steps}{'_normalized' if normalize else ''}_minadjusted"
 
 x_directory = f"../datasets/erlangen_dataset_minadjusted_gridlen{grid_element_length}.csv"
 y_directory = f"../datasets/erlangen_dataset_minadjusted_gridlen{grid_element_length}_label.csv"
-
-
 
 full_dataset = MeasurementDataset(x_directory=x_directory,
                                   y_directory=y_directory,
@@ -43,12 +40,15 @@ full_dataset = MeasurementDataset(x_directory=x_directory,
                                   num_prev_steps=num_prev_steps,
                                   augmentation_count=augmentation_count,
                                   augmentation_distance_m=augmentation_distance_m,
-                                  is_training=True
+                                  is_training=True,
+                                  normalize=normalize
                                   )
 
 train_size = int(len(full_dataset) * train_ratio)
-generator = t.Generator().manual_seed(42) # to have same split everytime, otherwise during restoration training dataset and validation datasets are mixed
-train_dataset, val_dataset = t.utils.data.random_split(full_dataset, [train_size, len(full_dataset) - train_size], generator=generator)
+generator = t.Generator().manual_seed(
+    42)  # to have same split everytime, otherwise during restoration training dataset and validation datasets are mixed
+train_dataset, val_dataset = t.utils.data.random_split(full_dataset, [train_size, len(full_dataset) - train_size],
+                                                       generator=generator)
 
 train_dl = t.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
 val_dl = t.utils.data.DataLoader(val_dataset, batch_size=batch_size, shuffle=True)
