5779ad93a06bea5001d1fbdaea836f419c4574fa
ardabuglagil <arda.buglagil@fau.de>
2023-06-14 16:47:38 +0200

    fix error - make parametric

--- nn_no_preaugmentation/visualize_losses.py
+++ nn_no_preaugmentation/visualize_losses.py
@@ -5,7 +5,21 @@ import glob
 
 def main():
     # parameters
-    checkpoint_name = "checkpoints/mlp_9_grid50_prev3_normalized_minadjusted"
+    CHECKPOINT_FOLDER = "grid_search_checkpoints"
+    GRID_WIDTH = 800
+    GRID_HEIGHT = 800
+    grid_element_length = 50
+    normalize = True
+    augmentation_count = 0
+    augmentation_distance_m = 3
+    # num_prev_steps_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    # input_features_list = [6, 9, 12, 15, 18]
+    input_features = 6
+    num_prev_steps = 10
+
+
+    checkpoint_name = f"{CHECKPOINT_FOLDER}/mlp_{input_features}_grid{grid_element_length}_prev{num_prev_steps}{'_normalized' if normalize else ''}_minadjusted{'_augmented' + str(augmentation_count) + '-' + str(augmentation_distance_m) if augmentation_count > 0 else ''}"
+
     # parameters
 
     files = sorted(glob.glob(checkpoint_name + '/*'))
@@ -22,7 +36,7 @@ def main():
     plt.legend()
     plt.xlabel("Epochs")
     plt.ylabel("Loss")
-    plt.title(checkpoint_folder.split("/")[1])
+    plt.title(checkpoint_name.split("/")[1])
     plt.show()
 
 
