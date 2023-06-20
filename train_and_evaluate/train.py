470dd762315347771efa0ee825cdaf0e740f743d
ardabuglagil <arda.buglagil@fau.de>
2023-06-20 10:11:40 +0200

    parametric model selection

--- train_and_evaluate/train.py
+++ train_and_evaluate/train.py
@@ -5,16 +5,14 @@ from trainer import Trainer
 from matplotlib import pyplot as plt
 import numpy as np
 from nn.mlp import Mlp
+from lstm.lstm import LSTMModel
 from utils import generate_combinations
 
-
-CHECKPOINT_FOLDER = "grid_search_checkpoints"
-
+model_type = "lstm"  # lstm, mlp
+CHECKPOINT_FOLDER = f"grid_search_checkpoints_{model_type}"
 restored_checkpoint = -1  # -1 for no restoration
 epochs = 1000  # train until this epoch
 early_stopping_patience = 30
-
-
 
 # dataset parameters
 GRID_WIDTH = 800
@@ -38,50 +36,70 @@ input_features_list = [6, 9, 12, 15, 18]
 parameter_lists = [num_prev_steps_list, input_features_list]
 parameter_combinations = generate_combinations(parameter_lists)
 
-for param_comb in parameter_combinations:
-    num_prev_steps = param_comb[0]
-    input_features = param_comb[1]
 
-    output_classes = int((GRID_WIDTH / grid_element_length) * (GRID_HEIGHT / grid_element_length))
-    network_input_length = num_prev_steps * input_features
+def main():
+    for param_comb in parameter_combinations:
+        num_prev_steps = param_comb[0]
+        input_features = param_comb[1]
 
-    checkpoint_name = f"{CHECKPOINT_FOLDER}/mlp_{input_features}_grid{grid_element_length}_prev{num_prev_steps}{'_normalized' if normalize else ''}_minadjusted{'_augmented'+str(augmentation_count)+'-'+str(augmentation_distance_m) if augmentation_count>0 else ''}"
+        output_classes = int((GRID_WIDTH / grid_element_length) * (GRID_HEIGHT / grid_element_length))
+        input_length = num_prev_steps * input_features
 
-    x_directory = f"../datasets/erlangen_dataset_minadjusted_gridlen{grid_element_length}.csv"
-    y_directory = f"../datasets/erlangen_dataset_minadjusted_gridlen{grid_element_length}_label.csv"
-    grid_line_directory = f"../datasets/erlangen_dataset_minadjusted_grid_lines_gridlen{grid_element_length}.json"
+        checkpoint_name = f"{CHECKPOINT_FOLDER}/mlp_{input_features}_grid{grid_element_length}_prev{num_prev_steps}{'_normalized' if normalize else ''}_minadjusted{'_augmented' + str(augmentation_count) + '-' + str(augmentation_distance_m) if augmentation_count > 0 else ''}"
 
-    full_dataset = MeasurementDataset(x_directory=x_directory,
-                                      y_directory=y_directory,
-                                      num_features=input_features,
-                                      num_prev_steps=num_prev_steps,
-                                      augmentation_count=augmentation_count,
-                                      augmentation_distance_m=augmentation_distance_m,
-                                      is_training=True,
-                                      normalize=normalize,
-                                      grid_line_directory=grid_line_directory
-                                      )
+        x_directory = f"../datasets/erlangen_dataset_minadjusted_gridlen{grid_element_length}.csv"
+        y_directory = f"../datasets/erlangen_dataset_minadjusted_gridlen{grid_element_length}_label.csv"
+        grid_line_directory = f"../datasets/erlangen_dataset_minadjusted_grid_lines_gridlen{grid_element_length}.json"
 
-    train_size = int(len(full_dataset) * train_ratio)
-    generator = t.Generator().manual_seed(
-        42)  # to have same split everytime, otherwise during restoration training dataset and validation datasets are mixed
-    train_dataset, val_dataset = t.utils.data.random_split(full_dataset, [train_size, len(full_dataset) - train_size],
-                                                           generator=generator)
+        full_dataset = MeasurementDataset(x_directory=x_directory,
+                                          y_directory=y_directory,
+                                          num_features=input_features,
+                                          num_prev_steps=num_prev_steps,
+                                          augmentation_count=augmentation_count,
+                                          augmentation_distance_m=augmentation_distance_m,
+                                          is_training=True,
+                                          normalize=normalize,
+                                          grid_line_directory=grid_line_directory
+                                          )
 
-    train_dl = t.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
-    val_dl = t.utils.data.DataLoader(val_dataset, batch_size=batch_size, shuffle=True)
+        train_size = int(len(full_dataset) * train_ratio)
+        generator = t.Generator().manual_seed(
+            42)  # to have same split everytime, otherwise during restoration training dataset and validation datasets are mixed
+        train_dataset, val_dataset = t.utils.data.random_split(full_dataset,
+                                                               [train_size, len(full_dataset) - train_size],
+                                                               generator=generator)
 
-    model = Mlp(input_features=network_input_length, output_classes=output_classes)
+        train_dl = t.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+        val_dl = t.utils.data.DataLoader(val_dataset, batch_size=batch_size, shuffle=True)
 
-    crit = t.nn.CrossEntropyLoss()
-    optim = t.optim.Adam(model.parameters(), lr=learning_rate)
+        model = choose_model(model_name=model_type, in_out_size=(input_length, output_classes))
 
-    trainer = Trainer(model, crit=crit, optim=optim, early_stopping_patience=early_stopping_patience, train_dl=train_dl, val_test_dl=val_dl,
-                      cuda=True, checkpoint_folder=checkpoint_name)
+        crit = t.nn.CrossEntropyLoss()
+        optim = t.optim.Adam(model.parameters(), lr=learning_rate)
 
-    trainer.restore_checkpoint(restored_checkpoint)
+        trainer = Trainer(model, crit=crit, optim=optim, early_stopping_patience=early_stopping_patience,
+                          train_dl=train_dl, val_test_dl=val_dl,
+                          cuda=True, checkpoint_folder=checkpoint_name)
 
-    res = trainer.fit(epochs=epochs)
+        trainer.restore_checkpoint(restored_checkpoint)
+
+        res = trainer.fit(epochs=epochs)
+
+
+def choose_model(model_name, in_out_size):
+    if model_name == "mlp":
+        model = Mlp(input_features=in_out_size[0], output_classes=in_out_size[1])
+    elif model_name == "lstm":
+        model = LSTMModel(input_dim=in_out_size[0], hidden_dim=32, layer_dim=1, output_dim=in_out_size[1])
+    else:
+        print("Wrong model name entered.")
+        model = None
+
+    return model
+
+
+if __name__ == "__main__":
+    main()
 
 # # plot the results
 # plt.plot(np.arange(len(res[0])), res[0], label='train loss')
